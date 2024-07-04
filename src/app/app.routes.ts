c4979492ac3e50af2839b9e4776f1454a7a9058f
sanidh-id <sanidh.id@polussolutions.com>
2024-07-04 17:46:34 +0530

    Added link to a basic Signup component (no styling)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: '**', redirectTo: '', pathMatch: 'full' },
-  { path: '', outlet: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
