3d9d3178e55f790433bc8adb4b7bab78eef1b0ef
sanidh-id <sanidh.id@polussolutions.com>
2024-07-04 16:13:02 +0530

    Added a basic Bootstrap login form & print the inputs to the console

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  login(username: string, password: string) {
+    // TypeScript code
+    if (username=="" || password =="") {
+        alert("Please enter valid details.");
+        return;
+    }
+    // Example: Just log the values to console
+    console.log('Username: ' + username);
+    console.log('Password: ' + password);
 
+    // You can perform further validation or processing here
+
+    // Example: Redirect to another page after successful login
+    // window.location.href = 'dashboard.html';
+  }
 }
