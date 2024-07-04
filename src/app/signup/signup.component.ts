95682c00d2e44598baaadb81ae952741627cd540
sanidh-id <sanidh.id@polussolutions.com>
2024-07-04 18:08:25 +0530

    Added button to go back to Login in SignUp page & fixed styles

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
