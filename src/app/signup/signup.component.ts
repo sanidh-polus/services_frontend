547769cf68b1894d6219b6252b39be6034268cf2
sanidh-id <sanidh.id@polussolutions.com>
2024-07-05 10:29:25 +0530

    Added the main fields in the sign-up page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,5 +9,23 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  // Function to toggle password visibility
+  togglePasswordVisibility(): void {
+    const passwordInput = document.getElementById(
+      'password'
+    ) as HTMLInputElement;
+    const icon = document.getElementById(
+      'togglePasswordButton'
+    ) as HTMLElement;
 
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      icon.classList.remove('bi-eye-slash');
+      icon.classList.add('bi-eye');
+    } else {
+      passwordInput.type = 'password';
+      icon.classList.remove('bi-eye');
+      icon.classList.add('bi-eye-slash');
+    }
+  }
 }
