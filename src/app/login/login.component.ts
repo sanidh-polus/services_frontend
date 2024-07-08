caf8564054426f1ca5f73fa449ee8838efdcdb12
sanidh-id <sanidh.id@polussolutions.com>
2024-07-08 12:05:35 +0530

    Checking success & error login status in the console

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,7 +19,6 @@ interface Login {
 })
 export class LoginComponent {
   loginDetails: Login[] = [];
-  errorMessage!: string;
 
   constructor(private data_service: DataService) {}
 
@@ -47,8 +46,8 @@ export class LoginComponent {
     // }
 
     // Example: Just log the values to console
-    console.log('Username: ' + email);
-    console.log('Password: ' + password);
+    // console.log('Username: ' + email);
+    // console.log('Password: ' + password);
 
     let loginBody = {
       "email": email,
@@ -58,10 +57,23 @@ export class LoginComponent {
     // You can perform further validation or processing here
     this.data_service.checkLoginDetails(loginBody).subscribe({
       next: (response) => {
-        console.log(response.body);
+        console.log(response);
+        console.log("Status: Success");
       },
-      error: (error: string) => {
-        this.errorMessage = error;
+      error: (e: any) => {
+        if (e.status == 200) {
+          console.log("Status: Correct details")
+          return;
+        }
+        if (e.status == 401) {
+          console.log("Status: Invalid credentials");
+          return;
+        }
+        if (e.status == 500) {
+          console.log("Status: Cannot check data, server error");
+          return;
+        }
+        console.log("Error: ", e.status, e.error);
       },
     });
 
