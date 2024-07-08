86161849ca10cb261f5f56eb7d4231d509da6a35
sanidh-id <sanidh.id@polussolutions.com>
2024-07-08 17:35:47 +0530

    Formatted TypeScript files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -31,6 +31,11 @@ export class LoginComponent {
   login(email: string, password: string) {
     let errorText = document.getElementById('error-text') as HTMLInputElement;
 
+    let loginBody = {
+      "email": email,
+      "emp_password": password
+    }
+
     // TypeScript code
     if (email == '' || password == '') {
       // alert('Please enter valid details');
@@ -53,35 +58,30 @@ export class LoginComponent {
     // Example: Just log the values to console
     // console.log('Username: ' + email);
     // console.log('Password: ' + password);
-
-    let loginBody = {
-      "email": email,
-      "emp_password": password
-    }
     
     // You can perform further validation or processing here
     this.data_service.checkLoginDetails(loginBody).subscribe({
       next: (response) => {
-        console.log(response);
-        console.log("Status: Success");
+        console.log('Response: ', response);
+        console.log('Status: Success');
       },
       error: (e: any) => {
         console.log(e);
         if (e.status == 200) {
-          console.log("Status: Correct details")
+          console.log('Status: Correct details');
           return;
         }
         if (e.status == 401) {
-          console.log("Status: Invalid credentials");
+          console.log('Status: Invalid credentials');
           errorText.innerHTML = 'Invalid credentials';
           return;
         }
         if (e.status == 500) {
-          console.log("Status: Cannot check data, server error");
+          console.log('Status: Cannot check data, server error');
           errorText.innerHTML = 'Cannot check data, server error';
           return;
         }
-        console.log("Error: ", e.status, e.error);
+        console.log('Error: ', e.status, e.error);
       },
     });
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,25 +9,6 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  // Function to toggle password visibility
-  togglePasswordVisibility(inputId: string): void {
-    const passwordInput = document.getElementById(inputId) as HTMLInputElement;
-    const icon = document.getElementById(
-      `toggle${inputId.charAt(0).toUpperCase() + inputId.slice(1)}Button`
-    ) as HTMLElement;
-    // console.log('Button Name:', `toggle${inputId.charAt(0).toUpperCase() + inputId.slice(1)}Button`);
-
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      icon.classList.remove('bi-eye-slash');
-      icon.classList.add('bi-eye');
-    } else {
-      passwordInput.type = 'password';
-      icon.classList.remove('bi-eye');
-      icon.classList.add('bi-eye-slash');
-    }
-  }
-
   isValidEmailFormat(email: string): boolean {
     // Regular expression for basic email validation
     const emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
@@ -106,5 +87,24 @@ export class SignupComponent {
 
     // Example: Redirect to another page after successful login
     // window.location.href = 'dashboard.html';
+  }
+
+  // Function to toggle password visibility
+  togglePasswordVisibility(inputId: string): void {
+    const passwordInput = document.getElementById(inputId) as HTMLInputElement;
+    const icon = document.getElementById(
+      `toggle${inputId.charAt(0).toUpperCase() + inputId.slice(1)}Button`
+    ) as HTMLElement;
+    // console.log('Button Name:', `toggle${inputId.charAt(0).toUpperCase() + inputId.slice(1)}Button`);
+
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      icon.classList.remove('bi-eye-slash');
+      icon.classList.add('bi-eye');
+    } else {
+      passwordInput.type = 'password';
+      icon.classList.remove('bi-eye');
+      icon.classList.add('bi-eye-slash');
+    }
   }
 }
