0e53195c857cb015d8eab1f094d5c26e4985f852
sanidh-id <sanidh.id@polussolutions.com>
2024-07-09 10:41:53 +0530

    Made login route the default path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
