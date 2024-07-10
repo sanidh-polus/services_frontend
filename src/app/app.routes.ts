c66da4e66a1af6fa6cd8be7dcdbc1e5f7dc4fd1f
sanidh-id <sanidh.id@polussolutions.com>
2024-07-10 16:42:39 +0530

    Created a new module & component for the User home dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserHomeComponent } from './user/user-home/user-home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'user/home', component: UserHomeComponent }
 ];
